44abe54190bd639b59dd320e1fc8e85a1b3f1b4b
faissal17 <faissalaoukacha@gmail.com>
2023-12-29 16:52:55 +0100

    add categorie schema and run resources

---
+++ product-service/src/categories/categories.service.ts
@@ -0,0 +1,28 @@
+import { DatabaseService } from './../../../order-service/src/database/database.service';
+import { Injectable } from '@nestjs/common';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+
+@Injectable()
+export class CategoriesService {
+  constructor(private readonly Databaseservice: DatabaseService) {}
+  create(createCategoryDto: CreateCategoryDto) {
+    return 'This action adds a new category';
+  }
+
+  findAll() {
+    return `This action returns all categories`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} category`;
+  }
+
+  update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    return `This action updates a #${id} category`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} category`;
+  }
+}
