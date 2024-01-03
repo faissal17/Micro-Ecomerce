e7b4060a93c991bee51697c64a748ead52e0a8fa
faissal17 <faissalaoukacha@gmail.com>
2024-01-03 16:19:13 +0100

    add database resources

--- order-service/src/app.module.ts
+++ order-service/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService,],
-  imports: [OrdersModule],
+  imports: [OrdersModule, DatabaseModule],
 })
 export class AppModule {}
