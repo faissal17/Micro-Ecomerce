7c60e14a239070eb3979e6d729c428ca6065761c
faissal17 <faissalaoukacha@gmail.com>
2024-01-03 15:56:15 +0100

    create resources for order service

--- order-service/src/app.module.ts
+++ order-service/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService,],
+  imports: [OrdersModule],
 })
 export class AppModule {}
