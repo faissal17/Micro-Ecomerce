42b6d4f08d80762375e7f40864603e5b0bc276c4
faissal17 <faissalaoukacha@gmail.com>
2024-01-29 15:10:26 +0100

    add cors

--- product-service/src/main.ts
+++ product-service/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  const PORT = 3001
+  await app.listen(PORT);
+
+  console.log("app running on port" ,PORT)
 }
 bootstrap();
